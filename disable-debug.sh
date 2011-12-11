91c3fcd416c4c336c44212d636728e947b8c0705
Luke Campbell <luke.s.campbell@gmail.com>
2011-12-11 13:19:17 -0500

    LOEP and cleaned up depracated files

--- disable-debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SED=`which sed`
-$SED s/"-D__DEBUG__"/""/ Makefile > Makefile.tmp
-mv Makefile.tmp Makefile
-make clean
-echo "done"
--- enable-debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SED=`which sed`
-$SED s/"-ansi"/"-ansi -D__DEBUG__"/ Makefile > Makefile.tmp
-mv Makefile.tmp Makefile
-make clean
-echo "done"
